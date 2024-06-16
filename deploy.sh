93790b2e664cc1490c4875a914867e2296b1602d
jomasuleymen <joma.suleymen@gmail.com>
2024-06-17 01:38:30 +0500

    update: github actions

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,6 @@
-log "Pulling the latest changes from the Git repository"
-
-git fetch
-git pull
-
 # Download dependencies
 log "Download dependencies"
 npm ci
-
 
 # Restart app
 pm2 restart ecosystem.config.json
